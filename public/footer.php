2df0d18c64b10d4089f64d2e24ca282bb4d5b1d5
weeklyd3 <timothywei.spam@gmail.com>
2021-07-22 15:10:30 -0700

    No ECHO

--- public/footer.php
+++ public/footer.php
@@ -16,12 +16,11 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-?><?php
-echo '<hr><footer>';
-echo '<center>';
-echo "<small>looks like you're at the end!</small>";
-echo '<p>Version: 1.0.21';
-echo '<br>Hi! :) Have a good time here chatting! Also read these links:</p>';
-echo '<a href="DCP.php">Data Collection Policy</a> - <a href="tos.php">Terms of Use</a>';
-echo '</center></footer>';
 ?>
+<hr><footer>
+<center>
+<small>looks like you're at the end!</small>
+<p>Version: 1.0.21
+<br>Hi! :) Have a good time here chatting! Also read these links:</p>
+<a href="DCP.php">Data Collection Policy</a> - <a href="tos.php">Terms of Use</a>
+</center></footer>
