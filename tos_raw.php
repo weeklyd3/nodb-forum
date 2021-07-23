9d5fc12d7dc0bc39f7e96e87aea65bffbde2c45f
weeklyd3 <timothywei.spam@gmail.com>
2021-07-22 22:37:00 -0700

    TOS change, just formatting, no content changed

--- tos_raw.php
+++ tos_raw.php
@@ -1,30 +1,11 @@
-<?php
-/*
-    Forum Software
-    Copyright (C) 2021 contributors
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-?><?php
-echo '<p> - This is a <i>forum</i>. This has user-submitted content. <i>You</i> are responsible for what you do, and other people are responsible for what they do.</p>';
-echo '<p> - User content is licensed under the <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>. By signing up, you agree to those terms.</p>';
-echo '<p> - You must give attribution as said by the license to the user who posted the message. Otherwise, you violate the license.</p>';
-echo "<p> - You may not impersonate someone else. Impersonation is strictly prohibited on this forum, and the moderators will handle any case of impersonation. Do not act as someone else just to fool others into believing another person said something.</p>";
-echo "<p> - Your personal data will not be submitted to third-parties. However, you take responsibility for the data you submit. The forum is not responsible for any data leaked during a data breach or hack. Similarly, you do not have to care about other users' data. You are not in charge of it.</p>";
-echo "<p> - You may not abuse others on the forum. We want to create a place where people can talk in respect and kindness, not yell and swear at each other. For this reason, you may not hack other users' data, for this same reason. <strong>This applies when you sign up.</strong></p>";
-echo '<p> - This Terms of Use document may be updated. We will show a notice that it will be updated on the Web site, along with the date on which it will be updated. By using the Web site beyond the listed date, you will agree that you agree to the new terms. If a new release is issued, all older copies are invalidated.</p>';
-echo '<p> - <strong>If you violate the Terms of Use, you will face:<ul><li>a possible account ban</li><li>account deletion</li><li>post deletion</li></ul>. Thank you for trying to make this forum a better place by abiding to this document when you sign up.</strong></p>';
-echo '<p> - This <i>Terms of Use</i> document governs use of the forums. This is the entire Terms of Use, and no other user can enforce more Terms of Use, neither another copy or a revision. If you agree, you may show your acceptance. <strong>If you do not agree to the Terms, do not use the services.</strong></p>';
-echo '<p> - Your username cannot be offensive. It also cannot hint that you are affiliated with any of the administrators, nor can it hint that you are an Administrator. Choose wisely!</p>'
-?>
+<p> - This is a <em>forum</em>. This has user-submitted content. <em>You</em> are responsible for what you do, and other people are responsible for what they do.</p>
+<p> - User content is licensed under the <a href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License</a>. By signing up, you agree to those terms.</p>
+<p> - You must give attribution as said in the license above by the license to the user who posted the message. Otherwise, you violate the license.</p>
+<p> - You may not impersonate someone else. Impersonation is strictly prohibited on this forum, and the moderators will handle any case of impersonation. Do not act as someone else just to fool others into believing another person said something.</p>
+<p> - Your personal data will not be submitted to third-parties. However, you take responsibility for the data you submit. The forum is not responsible for any data leaked during a data breach or hack. Similarly, you do not have to care about other users' data. You are not in charge of it.</p>
+<p> - You may not abuse others on the forum. We want to create a place where people can talk in respect and kindness, not yell and swear at each other. For this reason, you may not hack other users' data, for this same reason. <strong>This applies when you sign up.</strong></p>
+<p> - This Terms of Use document may be updated. We will show a notice that it will be updated on the Web site, along with the date on which it will be updated. By using the Web site beyond the listed date, you will agree that you agree to the new terms. If a new release is issued, all older copies are invalidated.</p>
+<p> - <strong>If you violate the Terms of Use, you will face:<ul><li>a possible account ban</li><li>account deletion</li><li>post deletion</li></ul>. Thank you for trying to make this forum a better place by abiding to this document when you sign up.</strong></p>
+<p> - This <em>Terms of Use</em> document governs use of the forums. This is the entire Terms of Use, and no other user can enforce more Terms of Use, neither another copy or a revision. If you agree, you may show your acceptance. <strong>If you do not agree to the Terms, do not use the services.</strong></p>
+<p> - Your username cannot be offensive. It also cannot hint that you are affiliated with any of the administrators, nor can it hint that you are an Administrator. Choose wisely!</p>
+Changed version, edited
