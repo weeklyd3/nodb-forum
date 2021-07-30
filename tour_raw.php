bd215fe32f4f7932a00079a41535a2fde02df8b0
weeklyd3 <timothywei.spam@gmail.com>
2021-07-30 16:17:54 -0700

    The tour

---
+++ tour_raw.php
@@ -0,0 +1,19 @@
+Thank you for joining our site! We'll go over some
+rules here:
+<hr />
+<h2>For the End Users</h2>
+<ul>
+<li><h3>There are real humans on the network!</h3> There are real humans 
+communicating with you! So don't be extra
+offensive to them. (And why would anybody want to release their spambot here?)</li>
+<li><h3>Get an account</h3>An account allows you to:<ol><li>Participate in Chat</li><li>Upload files</li><li>Create topics</ol> So it's a lot better to browse with an account! (Better yet, it's free and easy to do.)</li>
+<li><h3>Check out the <a href="faq.php">FAQ!</a></h3>Many questions are there, such as user licenses, why register, and more.</li>
+<li><h3>Pick an interesting topic...</h3>An established forum may already have many topics. Choose the one you would like to discuss!</li>
+<li><h3>...or start your own!</h3>If your forum is not that established, you may find that no topics suit your purpose. For that reason, you may click 'New Topic' (or go <a href="create.php">here</a>) to create a new topic.</li>
+</ul>
+<hr />
+<h2>For the Developers</h2>
+<ol>
+<li>Think of a feature that you wish you could have, or adopt on an existing one that has no assignees.</li>
+<li>Clone the repo and edit the code there. The repo code is occasionally broken, but we don't want that too often.</li>
+</ol>
