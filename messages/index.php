e6fc6739365100aa955d8cb3d0462dfd1a0f33f9
weeklyd3 <timothywei.spam@gmail.com>
2022-02-22 13:49:09 -0800

    Sorting for messages

--- messages/index.php
+++ messages/index.php
@@ -28,38 +28,94 @@
   </head>
   <body>
 	<?php 
+$GLOBALS['filterMethod'] = isset($_GET['filter']) ? $_GET['filter'] : "everything";
 	if (!isset($_COOKIE['login'])) {
-		?><h2>Messages</h2><p>Send private messages to other people, without using other applications or installing any software.</p><p>Free to use for anyone who has an account.</p><?php
+		?><h2>Messages</h2><p>Send private messages to other people, without using other applications or installing any software.</p><p>Free to use for anyone who has an account.</p><p>Register an account to send and receive mail.</p><?php
 	} else {
 		?>	<br /><div style="border-bottom:1px solid;">
 		<a href="account/">Account Options</a>
 		<span style="border-radius:3px 3px 0px 0px; text-decoration:none;border:1px solid;border-bottom:1px solid pink;">Private Messages</span>
 	</div><h2>Inbox</h2><div><a href="messages/new.php">Write message</a></div>
-		<table width="100%" class="table">
-			<tr><th>From</th><th>Subject</th><th>Preview</th><th>View</th></tr>
+	  <div>
+		  <?php 
+			function check(string $name) {
+				if ($GLOBALS['filterMethod'] === $name) {
+					?> selected="selected"<?php
+				}
+			}
+		?>
+		  <form action="messages/" method="get">
+			  <label>Messages to show:
+			  <select name="filter">
+				  <optgroup label="Everything">
+					  <option value="everything"<?php check('everything'); ?>>Show everything</option>
+				  </optgroup>
+				  <optgroup label="Filter messages to show">
+				  <option value="unread"<?php check('unread'); ?>>Show unread</option>
+				  <option value="read"<?php check('read'); ?>>Show read</option>
+				  </optgroup>
+			  </select>
+			  </label>
+			  <input type="submit" value="Show!" />
+		  </form>
+	  </div>
+		<table width="100%" class="messages table">
+			<tr class="msgheader"><th>From</th><th>Message</th><th>View</th></tr>
 			<?php 
+		$GLOBLALS['mailShown'] = 0;
 				if (!file_exists(__DIR__ . '/../data/accounts/' . cleanFilename(getname()) . '/msg.json')) {
 					fwrite(fopen(__DIR__ . '/../data/accounts/' . cleanFilename(getname()) . '/msg.json', 'w+'), "{}");
 				}
 				$r = array_reverse((array) json_decode(file_get_contents(__DIR__ . '/../data/accounts/' . cleanFilename(getname()) . '/msg.json')));
 				foreach ($r as $n => $m) {
+					if ($GLOBALS['filterMethod'] !== 'everything') {
+						if (count($m->read) > 0 && $GLOBALS['filterMethod'] === 'unread') continue;
+						if (count($m->read) === 0 && $GLOBALS['filterMethod'] === 'read') continue;
+					}
+						$GLOBALS['mailShown']++;
 					?><tr<?php 
 						if (!in_array(getname(), $m->read)) {
 							?> style="font-weight:bold;"<?php
 						}
 					?>>
-						<td><?php echo htmlspecialchars($m->from); ?></td>
-						<td><?php echo htmlspecialchars($m->subject); ?></td>
-						<td><?php 
-							echo htmlspecialchars(substr(str_replace(array("\r", "\n"), '', $m->body), 0, 300));
-						?></td>
+						<td><?php userlink($m->from); ?></td>
+						<td><a href="messages/viewmsg.php?id=<?php echo htmlspecialchars(urlencode($n)); ?>"><div class="bold big"><?php echo htmlspecialchars($m->subject); ?></div>
+						<?php 
+							echo htmlspecialchars(substr(str_replace(array("\r", "\n"), ' ', $m->body), 0, 30));
+					if (strlen(str_replace(array("\r", "\n"), ' ', $m->body)) > 30) {
+						?>...<?php
+					}
+						?></a></td>
 						<td><a href="messages/viewmsg.php?id=<?php echo htmlspecialchars(urlencode($n)); ?>">View</a></td>
 					</tr><?php
 				}
+		if (!$GLOBALS['mailShown']) {
+			?><tr><td colspan="3">
+				<h3>You have no messages.</h3>
+				<p>Try:</p>
+				<ul>
+					<li>Changing the filter option</li>
+					<li>Participating in the site to receive messages</li>
+					<li>Sending a message to someone else</li>
+				</ul>
+			</td></tr><?php
+		}
 			?>
-			<tr><th>From</th><th>Subject</th><th>Preview</th><th>View</th></tr>
+			<tr class="msgheader"><th>From</th><th>Message</th><th>View</th></tr>
 		</table>
 		<?php
 	}
 	include('../public/footer.php');
 	?>
+<style>
+	.bold {
+		font-weight: 700;
+	}
+	.big {
+		font-size: 18px;
+	}
+	.messages tr:not(.msgheader):hover {
+background-color: skyblue;
+		color: black;
+	}
+</style>
