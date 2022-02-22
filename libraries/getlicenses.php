fffe07d03b5810f922954aa9be4ef31cf3ca925c
weeklyd3 <timothywei.spam@gmail.com>
2022-02-22 13:49:31 -0800

    Licenses for file uploader

---
+++ libraries/getlicenses.php
@@ -0,0 +1,14 @@
+<?php 
+function getlicenses() {
+return array(
+	"Fair use" => "This file is copyrighted; however, the usage of this file qualifies as fair use. Any other use may be copyright infringement.",
+	"CC BY-SA 4.0 (author)" => "You are the author, and you license this work under the CC BY-SA 4.0.",
+	"CC BY-SA 4.0 (found it)" => "Found this work with a CC BY-SA 4.0 license. Required attribution and more information is in the details field.",
+	"Public domain (author dedication)" => "Author placed it in public domain. More information is in the details field.",
+	"Public domain (published before 1923)" => "This work was published before 1923 and is in the public domain. Evidence is in the details field.",
+	"Public domain (Author died 70+ years ago)" => "This work is more than 70 years plus the author's lifetime old. Evidence is provided in the details field.",
+	"Public domain (anonymous author, work is 120+ years old)" => "Proof of anonymous authorship and the age of this work is in the details field.",
+	"Public domain (not creative enough)" => "This file is not creative enough to be eligible for copyright and is therefore in the public domain.",
+	"Other license" => "This work is licensed under another free content license, and distribution is allowed. A link to the license, text to satisfy licensing conditions, and more details are in the details field."
+);
+}
