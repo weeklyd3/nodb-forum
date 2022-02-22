1a51c93a7f0631d4906ee1f7b6db1655059e061c
weeklyd3 <timothywei.spam@gmail.com>
2022-02-22 13:44:55 -0800

    File info

--- viewfile.php
+++ viewfile.php
@@ -25,9 +25,38 @@
 	?>
   </head>
   <body>
+	  <p><a href="files/directory.php">Back to All files</a></p>
+			  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+<label>Enter a filename to view the file:
+	<input type="text" name="filename" id="filename" />
+</label>
+				  
+<input type="submit" value="View file!" />
+				  
+<input type="submit" name="viewinfo" value="View file information!" />
+<script src="styles/other/picker.js"></script>
+<link rel="stylesheet" href="styles/other/picker.css" />
+<script>
+globalThis.noOpenButton = true;	initFilePicker(document.getElementById('filename'));</script>
+</form>
   <h2><?php 
   function toolbar($name) {
-	  ?><div class="toolbar-wrapper">
+	  ?><div class="toolbar-wrapper" style="color: black;">
+	  	<?php
+		  $fileMetadata = file_exists(__DIR__ . '/file_details.json') ? json_decode(file_get_contents(__DIR__ . '/file_details.json')) : json_decode("{}");
+		  if (isset($fileMetadata->$name)) { ?> 
+		  <div style="color: black;">
+		  <?php 
+		  if (isset($_GET['viewinfo'])) {
+		  ?>
+		  Viewing file information (<a href="viewfile.php?filename=<?php echo htmlspecialchars(urlencode($name)); ?>">view file</a>)
+		  <?php } else {
+		  ?>
+		  Viewing file (<a href="viewfile.php?filename=<?php echo htmlspecialchars(urlencode($name)); ?>&viewinfo=true">view file information</a>)<?php
+		    } ?>
+		  </div><?php 
+		  }
+		?>
 	  	<div class="name"><?php echo htmlspecialchars($name); ?> (<code><?php echo filesize($name); ?></code> bytes) <span class="float-right"><a download="<?php echo htmlspecialchars($name); ?>" href="files/download.php?filename=<?php echo htmlspecialchars($name); ?>">Download</a> <a href="files/download.php?filename=<?php echo htmlspecialchars($name); ?>" target="_blank">Open in new tab</a></span></div>
 		  <div class="file-contents"><?php 
 		  return true;
@@ -37,10 +66,39 @@
   chdir("files/uploads");
   if (!file_exists($n)) die("Bad name</h2>Bad filename");
   echo htmlspecialchars($n); ?></h2>
-  <p><a href="files/download.php?filename=<?php echo htmlspecialchars($n); ?>">Original file (click and press <kbd>Ctrl</kbd> + <kbd>S</kbd> to download)</a></p>
+  <p><a href="files/download.php?filename=<?php echo htmlspecialchars($n); ?>">Original file (click and press <kbd>Ctrl/Command</kbd> + <kbd>S</kbd> to download)</a></p>
   <p>Type: <?php 
   $type = mime_content_type($n);
-  echo htmlspecialchars($type); ?></p><p>This file was detected as: <?php
+  echo htmlspecialchars($type); ?></p><?php if (!isset($_GET['viewinfo'])) { ?><p>This file was detected as: <?php }
+  if (isset($_GET['viewinfo'])) {
+	  $fileMetadata = file_exists(__DIR__ . '/file_details.json') ? json_decode(file_get_contents(__DIR__ . '/file_details.json')) : json_decode("{}");
+	  toolbar($n);
+	  if (!isset($fileMetadata->$n)) {
+		  ?>No file info available.<?php
+	  } else {
+		  $filedata = $fileMetadata->$n;
+		  ?>
+		  <dl>
+		  <dt>
+		  Uploader</dt>
+		  <dd>
+		  <?php echo htmlspecialchars($filedata->uploader); ?>
+		  </dd>
+		  <dt>Licensing</dt>
+		  <dd>
+		  <?php echo htmlspecialchars($filedata->license); ?>
+		  </dd>
+		  <dt>More information about license</dt>
+		  <dd><?php echo htmlspecialchars($filedata->extendedLicense); ?></dd>
+		  <dt>File details</dt>
+		  <dd><?php
+		  $Parsedown = new Parsedown;
+		  echo $Parsedown->text($filedata->details); ?></dd>
+		  </dl>
+		  <?php
+	  }
+	  ?></div></div><?php
+  } else {
   if (startsWith($type, "image/")) {
 	  ?>image.</p>
 	  <?php $GLOBALS['t'] = toolbar($n); ?>
@@ -82,4 +140,4 @@
   }
   ?><style>.file-contents { overflow: scroll; max-height: 90vh; }</style><?php
   if (isset($GLOBALS['t'])) { ?></div></div><?php }
-  else { ?>File type not supported. Click the link above to download the file.<?php }
+  else { ?>File type not supported. Click the link above to download the file.<?php } }
