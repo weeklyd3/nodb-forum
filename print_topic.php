b6e2d35b2fb6575f654ff736bb9ac8935cf98d79
weeklyd3 <timothywei.spam@gmail.com>
2022-02-22 13:48:41 -0800

    Improve printable versions

--- print_topic.php
+++ print_topic.php
@@ -1,7 +1,7 @@
 <?php
 /*
     Forum Software
-    Copyright (C) 2021 contributors
+    Copyright (C) 2022 contributors
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -16,13 +16,14 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-?><html lang="en">
+?><html lang="en" style="background-color: white; background: white;">
   <head>
     <title>Print Topic</title>
 	<?php
+require 'styles/inject.php';
 	?>
   </head>
-  <body>
+  <body style="background-color: white; background: white; color: black;">
 	<?php 
 	require_once __DIR__ . '/libraries/lib.php';
 	if (!isset($_GET['title'])) die("Specify title to print");
@@ -31,13 +32,30 @@
 	$config = json_decode(file_get_contents(__DIR__ . '/data/messages/' . cleanFilename($_GET['title']) . '/config.json'));
 	?>
 	<style>@media print {
-		#return-link {
-			display: none;
+		.return-link, #print-link {
+			display: none !important;
 		}
+	}
+	#print-link {
+		display: none;
 	}
 	</style>
 	<h1><?php echo htmlspecialchars($title); ?></h1>
-	<p>This is a printable version of the topic. <a id="return-link" href="viewtopic.php?room=<?php echo htmlspecialchars(urlencode($title)); ?>">Return to web version</a></p>
+	  <p>From <?php 
+$forumconfig = json_decode(file_get_contents('config.json'));
+echo htmlspecialchars($forumconfig->forumtitle); ?> (<i>http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST']); ?></i>)</p>
+	  <p>This topic can be found online at <i>http://<?php echo htmlspecialchars($_SERVER['HTTP_HOST']); ?>/viewtopic.php?room=<?php echo htmlspecialchars(urlencode($config->title)); ?></i></p>
+	<p class="return-link">This is a printable version of the topic. <a class="return-link" href="viewtopic.php?room=<?php echo htmlspecialchars(urlencode($title)); ?>">Return to web version</a>
+	<a class="return-link" id="print-link" href="javascript:;">Print now</a><br />
+	<span class="return-link smaller">
+	(Don't worry, neither this nor any of the links above will be printed!)</span>
+	<script>
+	document.querySelector('#print-link').style.display = 'inline';
+	document.querySelector('#print-link').addEventListener('click', function() {
+		window.print();
+	});
+	</script>
+	</p>
 	<h2>Tags</h2>
 	<p>Tagged: 
 	<?php 
@@ -46,17 +64,18 @@
 	}
 	?></p>
 	<h2>This Topic</h2>
-	<ol>
-		<li><strong><?php echo htmlspecialchars($config->author); ?></strong>
-		<blockquote style="border-left:5px solid;margin:15px;padding-left:15px;">
-			<?php echo $config->description_html; ?>
-		</blockquote></li>
+		<h3><?php userlink($config->author); ?> on <?php 
+		echo friendlyDate($config->creationTime); ?></h3>
+	  <div style="border:1px solid;padding:2px;">
+			<?php echo $config->description_html; ?></div>
 	<?php 
 		$msgs = (array) json_decode(file_get_contents(__DIR__ . '/data/messages/' . cleanFilename($title) . '/msg.json'));
 		foreach ($msgs as $msg) {
-			?><li><strong><?php echo file_exists(__DIR__ . '/data/accounts/' . cleanFilename($msg->author) . '/psw.txt') ? htmlspecialchars($msg->author) : "&lt;USER REMOVED>"; ?></strong>
-			<blockquote style="border-left:5px solid;margin:15px;padding-left:15px;">
-			<?php echo $msg->html; ?>
-			</blockquote></li><?php
+			?>
+			<h3><?php userlink($msg->author); ?> on <?php echo friendlyDate($msg->time); ?></h3>
+	  <div style="border: 1px solid; padding: 2px;">
+			<?php echo $msg->html; ?></div><?php
 		}
 	?>
+	<hr />
+	<p>End of topic</p>
