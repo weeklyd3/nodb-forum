0c9b53c9b0071f8fccb95ce3e70cfef5e275571c
weeklyd3 <timothywei.spam@gmail.com>
2022-02-22 13:44:33 -0800

    Change downloaded file name using `Content-Disposition`
    Normally, when downloading, the <a> tag has a download="filename..." attribute that will set the correct download filename.
    
    But the filename changes to 'download.php' when downloading via the URL and pressing Ctrl-S.
    
    This commit fixes that.

--- files/download.php
+++ files/download.php
@@ -4,5 +4,7 @@ if (!isset($_GET['filename'])) {
 }
 require_once '../libraries/lib.php';
 header("Content-Type: " . mime_content_type("uploads/" . cleanFilename($_GET['filename'])));
+$name = cleanFilename($_GET['filename']);
+header('Content-Disposition: inline; filename="' . $name . '"');
 readfile("uploads/" . cleanFilename($_GET['filename']));
 ?>
