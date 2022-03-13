83f2327095f2dc82509506bc81e57f9ad8b09a18
weeklyd3 <i.hate.spam.mail.here+github@gmail.com>
2022-03-13 11:38:48 -0700

    Remove line break that created extra unnecessary space

--- public/footer.php
+++ public/footer.php
@@ -24,7 +24,7 @@ QUICK LINKS: <br />
 <center>
 <small>looks like you're at the end!</small>
 <p>Our content is licensed under <a href="copyright.php">these licenses</a>.</p>
-<p><br>Hi! :) Have a good time here chatting! Also read these links:</p>
+<p>Hi! :) Have a good time here chatting! Also read these links:</p>
 <a href="DCP.php">Data Collection Policy</a> - <a href="tos.php">Terms of Use</a> - 
 <a href="info.php">PHP Information</a><hr />
 <?php echo "This page was last modified on ".date("F d Y H:i:s.", getlastmod() ); ?> <strong>All times are UTC.</strong>
